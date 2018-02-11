289915712d22a0746b4c8324bcfd35172c30b6dc
Christian Fei <crifei93@gmail.com>
2018-02-11 12:32:06 +0100

    blink

--- web/views/main.js
+++ web/views/main.js
@@ -3,6 +3,20 @@ var css = require('sheetify')
 
 var TITLE = 'mmonitor'
 
+const blinkPrefix = css`
+  @keyframes blink {
+    from {
+      background: #ffff3d;
+    }
+    to {
+      background: inherit;
+    }
+  }
+  :host {
+    animation: blink 2s ease-out;
+  }
+`
+
 const monitorPrefix = css`
   @keyframes fade-out {
     from {
@@ -17,11 +31,17 @@ const monitorPrefix = css`
     animation: fade-out 1s ease-out;
     border-left: 2em solid #f1f1f1;
   }
+  :host h1 {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
 `
 
 module.exports = view
 
+let iter = 0
 function view (state, emit) {
+  iter++
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
 
   const resultsAsList = Object.keys(state.results).reduce((acc, key) => acc.concat([{monitor: key, result: state.results[key]}]), [])
@@ -36,9 +56,13 @@ function view (state, emit) {
           <ul class="flex flex-wrap">
             ${resultsAsList.map(r => html`
               <li class="${monitorPrefix} pl5 pr5 mb3">
-                <h1 class="mb1" style="font-size: 2em;">${r.monitor}</h1>
-                <h1 class="mb1" style="font-size: 6em;">${r.result}</h1>
-                <i>Last updated ${new Date().toISOString()}</i>
+                <h1 class="mb1" style="font-size: 2em;">${r.title || r.monitor}</h1>
+                <h1 class="mb1" style="font-size: 5em;">
+                  ${printJSON(r.result)}
+                </h1>
+                <i class="${iter % 2 === 0 ? blinkPrefix : null}">
+                  Last updated ${new Date().toISOString()}
+                </i>
               </li>
             `)}
           </ul>
@@ -46,4 +70,19 @@ function view (state, emit) {
       </main>
     </body>
   `
+}
+
+function printJSON (json) {
+  if (typeof json === 'object') {
+    if (Array.isArray(json)) {
+      return html`
+        <span>
+        ${json
+          .map(j => Object.keys(j)
+            .reduce((acc, curr) => `${acc} ${j[curr]}`, '') + ' | ')}
+        </span>
+      `
+    }
+  }
+  return json
 }
